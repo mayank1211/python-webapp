84ef584e09983798ade578bc51e880e41490f89b
Mayank Patel <mayankkumar.patel1@nhs.net>
2022-01-24 11:12:13 +0000

    add additional crud pages

--- app.py
+++ app.py
@@ -3,7 +3,9 @@
 # Import the following the components from flask_login package (current_user, login_required, login_user, logout_user)
 # from flask_login import *
 # Import all the modules and features offered by flask
+from multiprocessing.connection import wait
 import os
+from time import sleep
 from flask import *
 from forms import RegistrationForm
 app = Flask(__name__)
@@ -28,13 +30,34 @@ def index():
     return render_template("index.html", title="Skills Finder")
 
 
-@app.route("/update", methods=["GET", "POST"])
-def profile():
+@app.route("/profile/<username>", methods=["GET"])
+def profile(username):
+    username = username
+    return render_template("profile.html", title="User's profile", username=username)
+
+
+@app.route("/delete_skill/<username>", methods=["GET"])
+def delete_skill(username):
+    return redirect(url_for('.profile', username=username))
+
+
+@app.route("/update_skill/<username>", methods=["GET", "POST"])
+def update_skill(username):
     if request.method == "POST":
         # Should be the name of the user logged in
-        return render_template("profile.html", title="Update User's Profile")
+        return render_template("update_skill.html", title="Update User's Profile", username=username)
     else:
-        return render_template("profile.html", title="Update User's Profile")
+        return render_template("update_skill.html", title="Update User's Profile", username=username)
+
+
+@app.route("/update_comment/<username>", methods=["GET", "POST"])
+def update_comment(username):
+    username = username
+    if request.method == "POST":
+        # Should be the name of the user logged in
+        return render_template("update_comment.html", title="Update User's Profile", username=username)
+    else:
+        return render_template("update_comment.html", title="Update User's Profile", username=username)
 
 
 @app.route("/manage/account", methods=["GET"])
