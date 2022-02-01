4dc89052091d448318daea40a6d8e9fa40474c9f
Mayank Patel <mayank.patel1211.mp@gmail.com>
2022-02-01 14:30:10 +0000

    Add docker files for heroku deployments (#3)

--- web_application/models.py
+++ web_application/models.py
@@ -4,6 +4,7 @@ from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, Dat
 from sqlalchemy import inspect
 from flask_login import UserMixin
 from web_application import app, db, login_manager
+import bcrypt
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -16,15 +17,14 @@ class Users(UserMixin, db.Model):
     Name = db.Column(db.String(80), nullable=True)
     Email = db.Column(db.String(120), nullable=True)
     Password = db.Column(db.String(120), nullable=True)
-    Role = db.Column(db.String(100), nullable=True)
+    JobRole = db.Column(db.String(100), nullable=True)
     CurrentTeam = db.Column(db.String(100), nullable=True)
     LastUpdatedAt = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    UserRole = db.Column(db.String(8), nullable=False, default="Standard")
 
     def get_id(self):
         """Return the user id to satisfy Flask-Login's requirements."""
         return self.Id
-
-
 
 class Skills(db.Model):
     """ Skills Model """
@@ -41,5 +41,42 @@ class Comments(db.Model):
     UserId = db.Column(db.Integer, db.ForeignKey('users.Id'), nullable=False)
     Comments = db.Column(db.String(120), nullable=True)
 
+def create_database_and_data():
+    db.create_all()
 
-db.create_all()
+    user = db.session.query(Users).filter_by(Email="mayank.patel@admin.com").first()
+
+    if not user:
+        hashedUserPassword = bcrypt.hashpw("admin", bcrypt.gensalt(12))
+        newUser = Users(
+            Name = "Mayank Patel",
+            Email = "mayank.patel@admin.com",
+            Password = hashedUserPassword,
+            JobRole = "Software Developer",
+            CurrentTeam = "NHS.UK - Service Profiles",
+        )
+        db.session.add(newUser)
+        db.session.commit()
+
+        registeredUser = db.session.query(Users).filter_by(Email="mayank.patel@admin.com").first()
+        
+
+        skill_one = Skills(
+            UserId = registeredUser.Id, 
+            SkillName = "Kubernetes",
+            SkillRating = 5
+        )
+        skill_two = Skills(
+            UserId = registeredUser.Id,
+            SkillName = "C# dotnet",
+            SkillRating = 4
+        )
+        comment = Comments(
+            UserId = registeredUser.Id,
+            Comments = "I also do performance testing for NHS.UK on varies Covid and Non-Covid related services."
+        )
+
+        db.session.add(comment)
+        db.session.add(skill_one)
+        db.session.add(skill_two)
+        db.session.commit()
--- web_application/routes.py
+++ web_application/routes.py
@@ -1,73 +1,69 @@
+import os
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 # Import login and authentication modules
 from flask_login import login_user, login_required, logout_user, LoginManager, current_user
 from sqlalchemy import create_engine, select, insert, update, delete
+# Password hashing.
+import bcrypt
 # Manual
 from web_application import app, db
 from web_application.models import Users, Skills, Comments
 
-# Password hashing.
-import os
-import bcrypt
-
+# Complete
 @app.route("/login", methods=["GET", "POST"])
 def login():
     errorMessage = ""
     if request.method == "POST":
-        userEmailInput = request.form.get("email")
-        
-        if not db.session.query(Users).filter_by(Email=userEmailInput).first():
+        # Check if the input email address matches any users email storeed in database, if not show error message to the user
+        if not db.session.query(Users).filter_by(Email=request.form.get("email")).first():
             errorMessage = "User does not exist, please register the user first."
         else:
-            userFound = db.session.query(Users).filter_by(Email=userEmailInput).first()
+            # if matching email if found try sign in the user only if both email address and password matches.
+            userFound = db.session.query(Users).filter_by(Email=request.form.get("email")).first()
             # Comparing the hashed password due to bcrypt.checkpw not working as expected.
-            hashedUserInputPassword = bcrypt.hashpw(request.form.get("password"), userFound.Password)
-            if (userFound.Password == hashedUserInputPassword):
+            if (userFound.Password == bcrypt.hashpw(request.form.get("password"), userFound.Password)):
                 login_user(userFound, True)
+                session.permanent = True
                 return redirect(url_for('.index'))
             else:
                 errorMessage = "Incorrect details, please check and try again later"
-
     return render_template("login.html", title="Login", errorMessage=errorMessage)
 
-
+# Complete
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for(".index"))
 
+    errorMessage=""
     if request.method == "POST":
-        userEmailInput = request.form.get("email")
-        if db.session.query(Users).filter_by(Email=userEmailInput).first():
-            # Redirect the useer to login page with a message stating they are already a member
-            userExists = True
+        if db.session.query(Users).filter_by(Email=request.form.get("email_address")).first():
+            errorMessage="Email entered alread exists."
         else:
             # Create the standard user
-            userExists = False
-            db.create_all()
-            hashedUserPassword = bcrypt.hashpw(request.form.get("password"), bcrypt.gensalt(12))
             newUser = Users(
                 Name = request.form.get("full_name"),
                 Email = request.form.get("email_address"),
-                Password = hashedUserPassword,
-                Role = request.form.get("role"),
+                Password = bcrypt.hashpw(request.form.get("password"), bcrypt.gensalt(12)),
+                JobRole = request.form.get("role"),
                 CurrentTeam = request.form.get("current_team"),
             )
             db.session.add(newUser)
             db.session.commit()
-        # Redirect the user to login page to sign with their details
-        return redirect(url_for(".login", userExists=userExists))
+            # Redirect the user to login page to sign with their details
+            return redirect(url_for(".login"))
     # Render the register form page on the GET request
-    return render_template("register.html", title="Register")
+    return render_template("register.html", title="Register", errorMessage=errorMessage)
 
+# Complete
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('.login'))
-    
-# Half Complete
+
+# Complete
 @app.route("/")
 @app.route("/index")
 @login_required
@@ -76,10 +72,80 @@ def index():
     skills = db.session.query(Skills).all()
     return render_template("index.html", title="Skills Finder", users=users, skills=skills)
 
-@app.route("/manage/account", methods=["GET"])
+# Complete
+@app.route("/my/account/<userId>", methods=["GET"])
 @login_required
-def manage_account():
-    return render_template("manage_account.html", title="Manage Your Profile")
+def my_account(userId):
+    user = db.session.query(Users).filter_by(Id=userId).first()
+    return render_template("my_account.html", title="Manage Your Profile", user=user)
+
+# Complete
+@app.route("/update/account/<userId>", methods=["GET", "POST"])
+@login_required
+def update_account(userId):
+    user = db.session.query(Users).filter_by(Id=userId).first()
+    if request.method == "POST":
+        user.Name = request.form.get("full_name")
+        user.Email = request.form.get("email_address")
+        user.JobRole = request.form.get("role")
+        user.CurrentTeam = request.form.get("current_team")
+
+        if request.form.get("password"):
+            hashedUserPassword = bcrypt.hashpw(request.form.get("password"), bcrypt.gensalt(12))
+            user.Password = hashedUserPassword
+
+        db.session.commit()
+        return redirect(url_for(".my_account", userId=userId))
+    return render_template("update_my_profile.html", title="Update Your Profile", user=user)
+
+# Complete - (Redirect based on url coming from)
+@app.route("/delete/account/<userId>", methods=["GET"])
+@login_required
+def delete_profile(userId):
+    if userId == current_user.Id or current_user.UserRole == "Admin":
+        db.session.query(Users).filter_by(Id=userId).delete()
+        db.session.query(Skills).filter_by(UserId=userId).delete()
+        db.session.query(Comments).filter_by(UserId=userId).delete()
+        db.session.commit()
+    return redirect(url_for(".index"))
+
+# Complete
+@app.route("/manage/accounts", methods=["GET"])
+@login_required
+def manage_accounts():
+    if current_user.UserRole != "Admin":
+        return redirect(url_for('.index'))
+
+    users = db.session.query(Users).all()
+    return render_template("manage_accounts.html", title="Manage Profile's", users=users)
+
+# Complete
+@app.route("/make/admin/<userId>", methods=["GET"])
+@login_required
+def make_admin(userId):
+    if current_user.UserRole != "Admin":
+        return redirect(url_for('.index'))
+
+    user = db.session.query(Users).filter_by(Id=userId).first()
+    user.UserRole = "Admin"
+    db.session.commit()
+    return redirect(url_for(".manage_accounts", userId=userId))
+
+# Complete
+@app.route("/remove/admin/<userId>", methods=["GET"])
+@login_required
+def remove_admin(userId):
+    if current_user.UserRole != "Admin":
+        return redirect(url_for('.index'))
+
+    print('ashdgjahsgdjasgd ', len(db.session.query(Users).filter_by(UserRole="Admin").all()))
+
+    # check if more than 1 admin exists if not then redirect user to manage_accounts page without any chances
+    if (len(db.session.query(Users).filter_by(UserRole="Admin").all()) > 1):
+        user = db.session.query(Users).filter_by(Id=userId).first()
+        user.UserRole = "Standard"
+        db.session.commit()
+    return redirect(url_for(".manage_accounts"))
 
 # Complete
 @app.route("/profile/<userId>", methods=["GET"])
@@ -104,7 +170,7 @@ def add_skill(userId):
         db.session.commit()
         return redirect(url_for(".profile", userId=userId))
     else:
-        return render_template("update_skill.html", title="Update User's Profile", userId=userId)
+        return render_template("add_skill.html", title="Update User's Profile", userId=userId)
 
 # Complete
 @app.route("/delete_skill/<userId>/<skillId>", methods=["GET"])
